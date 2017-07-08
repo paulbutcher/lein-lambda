24a8ae0486f407679d6069491927d61ba1cdb180
Paul Butcher <paul@paulbutcher.com>
2017-07-08 17:32:44 +0100

    Minor rejig

--- src/lein_lambda/lambda.clj
+++ src/lein_lambda/lambda.clj
@@ -2,7 +2,7 @@
   (:require [lein-lambda.s3 :as s3])
   (:use [amazonica.aws.lambda]))
 
-(defn function-config [{:keys [function-name handler memory-size timeout role description] :as config}]
+(defn- function-config [{:keys [function-name handler memory-size timeout role description] :as config}]
   {:function-name function-name
    :handler handler
    :memory-size memory-size
@@ -10,16 +10,24 @@
    :role (role :arn)
    :runtime "java8"
    :description description
-   :code {:s3-bucket (s3/bucket-name config)}
-         :s3-key (s3/bucket-key)})
+   :code {:s3-bucket (s3/bucket-name config)
+          :s3-key (s3/bucket-key)}})
 
-(defn function-exists? [{:keys [function-name]}]
+(defn- function-exists? [{:keys [function-name]}]
   (try
     (get-function :function-name function-name)
     (catch Exception _ false)))
 
+(defn- deploy-create [function-config]
+  (println "Creating lambda function" (function-config :function-name))
+  (create-function function-config))
+
+(defn- deploy-update [function-config]
+  (println "Updating lambda function" (function-config :function-name))
+  (update-function-configuration function-config))
+
 (defn deploy [config]
   (let [function-config (function-config config)]
     (if (function-exists? function-config)
-      (update-function-configuration function-config)
-      (create-function function-config))))
+      (deploy-update function-config)
+      (deploy-create function-config))))
