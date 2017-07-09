31f18404d38aabd1bd4748a61b30aaa6dfb83ffa
Paul Butcher <paul@paulbutcher.com>
2017-07-09 11:07:17 +0100

    Check for existing method

--- src/lein_lambda/apigateway.clj
+++ src/lein_lambda/apigateway.clj
@@ -25,22 +25,33 @@
     [(find-path root-path resources)
      (find-path proxy-path resources)]))
 
-(defn- maybe-create-proxy-resource [api-id]
-  (let [[root-id proxy-id] (get-resource-ids api-id)]
-    (or proxy-id
-        (:id (amazon/create-resource :restapi-id api-id
-                                     :parent-id root-id
-                                     :path-part proxy-path-part)))))
+(defn- maybe-create-proxy-resource [api-id root-id proxy-id]
+  (or proxy-id
+    (:id (amazon/create-resource :restapi-id api-id
+                                 :parent-id root-id
+                                 :path-part proxy-path-part))))
+
+(def http-method "ANY")
+
+(defn- find-method [api-id proxy-id]
+  (try
+    (amazon/get-method :http-method http-method
+                      :resource-id proxy-id
+                      :restapi-id api-id)
+    (catch Exception _ false)))
 
 (defn- maybe-create-method [api-id proxy-id]
-  (amazon/put-method :restapi-id api-id
-                     :resource-id proxy-id
-                     :http-method "ANY"
-                     :authorization-type "NONE"
-                     :request-parameters {"method.request.path.proxy" true}))
+  (or
+    (find-method api-id proxy-id)
+    (amazon/put-method :restapi-id api-id
+                      :resource-id proxy-id
+                      :http-method http-method
+                      :authorization-type "NONE"
+                      :request-parameters {"method.request.path.proxy" true})))
 
 (defn deploy [{{:keys [name]} :api-gateway}]
   (when name
     (let [api-id (maybe-create-api name)
-          proxy-id (maybe-create-proxy-resource api-id)
-          method-id (maybe-create-method api-id proxy-id)])))    
+          [root-id proxy-id] (get-resource-ids api-id)]
+      (let [proxy-id (maybe-create-proxy-resource api-id root-id proxy-id)
+            method-id (maybe-create-method api-id proxy-id)]))))
