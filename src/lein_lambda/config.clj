ee8306e9d5174b975181f14c67f4a5cbbdf7591f
Paul Butcher <paul@paulbutcher.com>
2017-07-22 14:16:49 +0100

    Credentials can be overridden per-stage

--- src/lein_lambda/config.clj
+++ src/lein_lambda/config.clj
@@ -4,10 +4,10 @@
             [amazonica.core :as amazonica]))
 
 (def BaseConfig
-  {(s/optional-key :access-key) s/Str
-   (s/optional-key :secret-key) s/Str
-   (s/optional-key :endpoint) s/Str
-   (s/optional-key :region) s/Str
+  {(s/optional-key :credentials) {(s/optional-key :access-key) s/Str
+                                  (s/optional-key :secret-key) s/Str
+                                  (s/optional-key :endpoint) s/Str
+                                  (s/optional-key :region) s/Str}
    (s/optional-key :function) {(s/optional-key :name) s/Str
                                (s/optional-key :handler) s/Str
                                (s/optional-key :description) s/Str
@@ -21,16 +21,22 @@
 (def Config (merge BaseConfig
                    {:stages {s/Str BaseConfig}}))
 
-(defn get-config [{:keys [access-key secret-key endpoint region] :as config} stage]
+(defn- get-config* [config stage]
   (s/validate Config config)
-  (amazonica/defcredential (cond-> {}
-                             access-key (assoc :access-key access-key)
-                             secret-key (assoc :secret-key secret-key)
-                             endpoint (assoc :endpoint endpoint)
-                             region (assoc :endpoint region)))
   (let [stage-config (get-in config [:stages stage])
         merge-config #(merge (config %) (stage-config %))]
-    {:function    (merge-config :function)
+    {:credentials (merge-config :credentials)
+     :function    (merge-config :function)
      :s3          (merge-config :s3)
      :api-gateway (merge-config :api-gateway)
      :warmup      (merge-config :warmup)}))
+
+(defn get-config [config stage]
+  (let [c (get-config* config stage)
+        {{:keys [access-key secret-key endpoint region]} :credentials} c]
+    (amazonica/defcredential (cond-> {}
+                              access-key (assoc :access-key access-key)
+                              secret-key (assoc :secret-key secret-key)
+                              endpoint (assoc :endpoint endpoint)
+                              region (assoc :endpoint region)))
+    c))
