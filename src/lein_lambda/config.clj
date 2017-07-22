6354536b4c137035a672a76e189636999b4151c0
Paul Butcher <paul@paulbutcher.com>
2017-07-22 13:53:34 +0100

    Allow credentials to be specified in configuration

--- src/lein_lambda/config.clj
+++ src/lein_lambda/config.clj
@@ -1,9 +1,14 @@
 (ns lein-lambda.config
   (:require [schema.core :as s]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [amazonica.core :as amazonica]))
 
 (def BaseConfig
-  {(s/optional-key :function) {(s/optional-key :name) s/Str
+  {(s/optional-key :access-key) s/Str
+   (s/optional-key :secret-key) s/Str
+   (s/optional-key :endpoint) s/Str
+   (s/optional-key :region) s/Str
+   (s/optional-key :function) {(s/optional-key :name) s/Str
                                (s/optional-key :handler) s/Str
                                (s/optional-key :description) s/Str
                                (s/optional-key :memory-size) s/Int
@@ -16,8 +21,13 @@
 (def Config (merge BaseConfig
                    {:stages {s/Str BaseConfig}}))
 
-(defn get-config [config stage]
+(defn get-config [{:keys [access-key secret-key endpoint region] :as config} stage]
   (s/validate Config config)
+  (amazonica/defcredential (cond-> {}
+                             access-key (assoc :access-key access-key)
+                             secret-key (assoc :secret-key secret-key)
+                             endpoint (assoc :endpoint endpoint)
+                             region (assoc :endpoint region)))
   (let [stage-config (get-in config [:stages stage])
         merge-config #(merge (config %) (stage-config %))]
     {:function    (merge-config :function)
