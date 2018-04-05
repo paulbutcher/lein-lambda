685e476ac667fd498da96aed43844f1883c26e0f
Tom McMillen <tom.mcmillen@intilery.com>
2018-04-05 10:06:07 +0100

    Use STS to get the AWS account ID
    This is more reliable than using the iam call, as it will work with
    assumed roles and doesn't rely on a user having IAM access.

--- src/lein_lambda/identitymanagement.clj
+++ src/lein_lambda/identitymanagement.clj
@@ -1,14 +1,10 @@
 (ns lein-lambda.identitymanagement
   (:require [clojure.string :as string]
-            [amazonica.aws.identitymanagement :as amazon]))
+            [amazonica.aws.identitymanagement :as amazon]
+            [amazonica.aws.securitytoken :as sts]))
 
-; Sadly, this seems to be the only way to get the account ID :-(
-; https://stackoverflow.com/questions/10197784/how-can-i-deduce-the-aws-account-id-from-available-basicawscredentials
 (defn account-id []
-  (-> (amazon/get-user)
-    (get-in [:user :arn])
-    (string/split #":")
-    (nth 4)))
+  (get-in (sts/get-caller-identity {}) [:account]))
 
 (defn- get-role-arn [name]
   (get-in (amazon/get-role :role-name name) [:role :arn]))
